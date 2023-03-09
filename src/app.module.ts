7d8109995af3fd8d49005508cb7305f185b1ec4a
Nayahto <nayahto.147@gmail.com>
2023-03-09 16:47:45 -0300

    feat added: (genre)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { GameModule } from './game/game.module';
 import { ProfileModule } from './profile/profile.module';
+import { GenreModule } from './genre/genre.module';
 
 @Module({
-  imports: [UserModule, GameModule, ProfileModule],
+  imports: [UserModule, GameModule, ProfileModule, GenreModule],
   controllers: [AppController],
   providers: [AppService],
 })
