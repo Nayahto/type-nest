a5fd0734d98425ee3869fb9d52d0710449cba172
Nayahto <nayahto.147@gmail.com>
2023-03-09 17:50:31 -0300

    feat updated: (genre) class-validator

--- src/genre/dto/create-genre.dto.ts
+++ src/genre/dto/create-genre.dto.ts
@@ -1,6 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateGenreDto {
   id: string;
+  @ApiProperty({ example: 'aventura' })
   Name: string;
+  @ApiProperty({ example: 'fd84cdac-8bfd-4b03-b64c-0caa7360ebcc' })
   userId: string;
+  @ApiProperty({
+    example: [
+      '1f1051b1-9192-4b56-9073-17aca1206e8b',
+      '0beefcc7-98d9-4c6e-aa75-1821517dbef2',
+    ],
+  })
   gameId: string[];
 }
