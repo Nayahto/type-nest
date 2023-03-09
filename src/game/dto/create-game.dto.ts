407d9babbc466b6aa175c222fa8a57e938daa7b5
Nayahto <nayahto.147@gmail.com>
2023-03-09 10:22:50 -0300

    feat added: (game, user) class-validator

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,22 +1,47 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString, Max, Min } from 'class-validator';
 
 export class CreateGameDto {
   id: string;
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty({ example: 'red dead' })
   Title: string;
+
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty({
     example:
       'https://news.microsoft.com/wp-content/uploads/prod/sites/42/2020/04/RDR_XBOX_1920X1080-WIRE-5ea198f09b883-960x640.jpg',
   })
   CoverImageUrl: string;
+
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty({ example: 'a brief description about the game' })
   Description: string;
+
+  @IsNotEmpty()
+  @Min(2000)
+  @Max(2100)
+  @IsNumber()
   @ApiProperty({ example: 2018 })
   Year: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  @Max(5)
   @ApiProperty({ example: 4.5 })
   ImdbScore: number;
+
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty({ example: 'https://www.youtube.com/watch?v=WDvFz2LL1WI' })
   TrailerYouTubeUrl: string;
+
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty({
     example: 'https://www.youtube.com/watch?v=BoohYJlRXa8&t=2065s',
   })
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,15 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
   id?: string;
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty({ example: '000.000.000-00' })
   CPF: string;
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty({ example: '1234567' })
   Password: string;
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty({ example: 'chiquinho' })
   userName: string;
+  @IsNotEmpty()
+  @IsEmail()
   @ApiProperty({ example: 'chiquinho@gmail.com' })
   email: string;
+  @IsNotEmpty()
+  @IsString()
   @ApiProperty({ example: 'admin' })
   role: string;
 }
