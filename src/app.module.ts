44879c78b7d4c4eb94c8c4e41634b10c4f36ad02
Nayahto <nayahto.147@gmail.com>
2023-03-10 12:22:55 -0300

    feat added: (favorite)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { GameModule } from './game/game.module';
 import { ProfileModule } from './profile/profile.module';
 import { GenreModule } from './genre/genre.module';
+import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
-  imports: [UserModule, GameModule, ProfileModule, GenreModule],
+  imports: [UserModule, GameModule, ProfileModule, GenreModule, FavoriteModule],
   controllers: [AppController],
   providers: [AppService],
 })
